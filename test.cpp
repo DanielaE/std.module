690976cb9636de5ce8b83e335525ee0ee19c52c2
Daniela Engert <dani@ngrt.de>
2023-09-10 12:02:03 +0200

    Support the (obviously lacking!!) mini test app on more platforms

--- test.cpp
+++ test.cpp
@@ -3,21 +3,32 @@
 import std;
 
 #ifdef __GLIBCXX__
-static constexpr auto lib = "libstdc++";
+static constexpr auto Lib = "libstdc++";
+#elif defined (_MSVC_STL_UPDATE)
+static constexpr auto Lib = "ms-stl";
 #else
-static constexpr auto lib = "libc++";
+static constexpr auto Lib = "libc++";
 #endif
+#if defined (_MSC_VER)
+static constexpr auto Compiler = "MSVC";
+static constexpr auto Major = _MSC_VER;
+#else
+static constexpr auto Compiler = "Clang";
+static constexpr auto Major = __clang_major__;
+#endif
+
+auto g() -> std::generator<std::string> {
+    co_yield Compiler;
+    co_yield "import std;";
+};
+
 
 int main() {
     std::expected<int, bool> exp = 42;
-    const auto g = [] -> std::generator<std::string> {
-        co_yield "Clang";
-        co_yield "import std;";
-    };
     std::vector<std::string> vs;
     for (auto && s : g()) {
         vs.push_back(std::move(s));
     }
-    std::println("{} {} & {} welcome '{}' !", vs[0], __clang_major__, lib, vs[1]);
+    std::println("{} {} & {} welcome '{}' !", vs[0], Major, Lib, vs[1]);
     return vs.empty() + *exp;
 }
